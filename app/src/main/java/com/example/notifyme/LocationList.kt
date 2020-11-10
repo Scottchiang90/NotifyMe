0c0f6441aa4c4c74992c6b854c77e26bbd2752e3
Scott Chiang <39974069+SChiang-VA@users.noreply.github.com>
2020-11-10 23:01:40 +0800

    migrate locations from string to Set<String>

--- app/src/main/java/com/example/notifyme/LocationList.kt
+++ app/src/main/java/com/example/notifyme/LocationList.kt
@@ -1,6 +1,7 @@
 package com.example.notifyme
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
@@ -10,6 +11,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class LocationList : AppCompatActivity() {
+    // get shared preference
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +20,43 @@ class LocationList : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {saveLocation()}
-        displayLocations ()
+        // set shared preference
+        this.sharedPref = this.getSharedPreferences(getString(R.string.shared_pref), Context.MODE_PRIVATE)
+        migrateLocations()
+        displayLocations()
+    }
+
+    private fun migrateLocations(){
+        if (sharedPref.getStringSet(getString(R.string.shared_pref_locations), mutableSetOf<String>()).isNullOrEmpty()) {
+            // fetch location list
+            val locations = mutableListOf<String>()
+            for (x in 1..20) {
+                val loc = sharedPref.getString("location$x", "")
+                if (!loc.isNullOrBlank()) {
+                    locations.add(loc)
+                }
+            }
+            // save to Set<String>
+            with(sharedPref.edit()) {
+                putStringSet(getString(R.string.shared_pref_locations), locations.toMutableSet())
+                apply()
+            }
+        }
     }
 
     private fun saveLocation() {
-        // get shared preference
-        val sharedPref = this.getSharedPreferences( getString(R.string.shared_pref), Context.MODE_PRIVATE ) ?: return
-        // save
-        with (sharedPref.edit()) {
-            for (x in 1..20) {
-                putString("location$x",
-                findViewById<EditText>(resources.getIdentifier("location$x", "id", packageName))
-                    .text.toString())
-
+        // generate location list
+        val locations = mutableListOf<String>()
+        for (x in 1..20) {
+            val location = findViewById<EditText>(resources.getIdentifier("location$x", "id", packageName))
+                    .text.toString()
+            if (location.isNotBlank()){
+                locations.add(location)
             }
+        }
+        // save location list
+        with (sharedPref.edit()) {
+            putStringSet(getString(R.string.shared_pref_locations), locations.toMutableSet())
             apply()
         }
         Toast.makeText(this, "Gym Watchlist Saved!", Toast.LENGTH_SHORT).show()
@@ -38,16 +64,16 @@ class LocationList : AppCompatActivity() {
     }
 
     private fun displayLocations () {
-        // get shared preference
-        val sharedPref = this.getSharedPreferences(
-            getString(R.string.shared_pref),
-            Context.MODE_PRIVATE
-        ) ?: return
-
-        //fetch and set the list
+        // clear the list
         for (x in 1..20) {
             findViewById<EditText>(resources.getIdentifier("location$x", "id", packageName))
-                .setText(sharedPref.getString("location$x", ""))
+                    .setText("")
+        }
+        //fetch and set the list
+        val locations = sharedPref.getStringSet(getString(R.string.shared_pref_locations), mutableSetOf<String>())?: mutableSetOf<String>()
+        locations.forEachIndexed { index, location ->
+            findViewById<EditText>(resources.getIdentifier("location${index+1}", "id", packageName))
+                .setText(location)
         }
     }
 }
